9035f982216d113b87e153c0af18cfb1fecc2b3f
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-20 21:26:54 +0300

    20.04.2025

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ function App() {
       <button
         onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
         className="btn-fake-dark-mode"
-        >
+      >
         {isFakeDark ? "☀️" : "🌙"}
       </button>
-        {/* 2) PROVIDE VALUE TO CHILD COMPONENTS */}
+      {/* 2) PROVIDE VALUE TO CHILD COMPONENTS */}
       <PostProvider>
         <Header />
         <Main />
@@ -40,7 +40,7 @@ function App() {
 
 function Header() {
   //3) CONSUMING CONTEXT VALUE
-  const { onClearPosts } =usePosts()
+  const { onClearPosts } = usePosts();
 
   return (
     <header>
@@ -58,7 +58,7 @@ function Header() {
 
 function SearchPosts() {
   //3) CONSUMING CONTEXT VALUE
-  const { searchQuery, setSearchQuery } = usePosts()
+  const { searchQuery, setSearchQuery } = usePosts();
   return (
     <input
       value={searchQuery}
@@ -69,7 +69,7 @@ function SearchPosts() {
 }
 
 function Results() {
-  const { posts } = usePosts()
+  const { posts } = usePosts();
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
@@ -91,7 +91,7 @@ function Posts() {
 }
 
 function FormAddPost() {
-  const { onAddPost } = usePosts()
+  const { onAddPost } = usePosts();
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
@@ -122,7 +122,7 @@ function FormAddPost() {
 }
 
 function List() {
-  const { posts } = usePosts()
+  const { posts } = usePosts();
 
   return (
     <ul>
@@ -137,8 +137,8 @@ function List() {
 }
 
 function Archive() {
-  const { onAddPost } = usePosts()
-  
+  const { onAddPost } = usePosts();
+
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
--- src/PostContext.jsx
+++ src/PostContext.jsx
@@ -48,10 +48,11 @@ function PostProvider({ children }) {
   );
 }
 
-function usePosts(){
-    const context = useContext(PostContext)
-    if(context === undefined) throw new Error('postContext was used outside of the PostProvider')
-    return context
+function usePosts() {
+  const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("postContext was used outside of the PostProvider");
+  return context;
 }
 
 export { PostProvider, usePosts };
