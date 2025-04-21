386eeb34c08bb8435ce50850354040c5d000c73e
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-21 16:38:50 +0300

    21.04.2025

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import App from "./App";
+import AppMemo from "./App-Memo";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppMemo/>
   </React.StrictMode>
 );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import App from "./App.jsx";
+
+import AppMemo from "./App-Memo.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AppMemo />
   </StrictMode>
 );
